015a9ddc42b3108ccb554556b68554667f7cdddf
susanxia1006 <susanxia1006@gmail.com>
2018-02-04 15:01:14 -0500

    Set up the header component.

--- youtube-search/App.js
+++ youtube-search/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Hi I want to make even more changes</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1, backgroundColor: '#ddd' }}>
+        <Header
+
+          centerComponent={{ text: 'Youtube Search', style: { color: '#fff' } }}
+          outerContainerStyles={{ backgroundColor: '#E62117' }}
+
+        />
+      {/*Search bar*/}
+      {/*List of videos*/}
+
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
