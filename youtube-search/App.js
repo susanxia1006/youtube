cbb440cb70909edd16a20ad6c7830fb8e00c96eb
susanxia1006 <susanxia1006@gmail.com>
2018-02-05 12:34:09 -0500

    Separate out the SearchBar component

--- youtube-search/App.js
+++ youtube-search/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
-import { Header, Button } from 'react-native-elements';
+import { View } from 'react-native';
+import { Header } from 'react-native-elements';
+import { SearchBar } from './src/components';
 
 export default class App extends Component {
-  state = {
-    searchTerm: ''
-
-  };
 
   render() {
-    const {
-      searchContainer,
-      searchText,
-      searchButton
-    } = styles;
-
     return (
       <View style={{ flex: 1, backgroundColor: '#ddd' }}>
         <Header
@@ -23,45 +14,10 @@ export default class App extends Component {
           outerContainerStyles={{ backgroundColor: '#E62117' }}
 
         />
-      <View style={searchContainer}>
-        <TextInput
-          style={searchText}
-          onChangeText={searchInput => this.setState({ searchTerm: searchInput })}
-          value={this.state.searchTerm}
-        />
-        <Button
-          buttonStyle={searchButton}
-          title="Submit"
-          onPress={() => {
-            console.log(this.state.searchTerm);
-            console.log('button was pressed!');
-          }}
-        />
-      </View>
+      <SearchBar />
       {/*List of videos*/}
 
       </View>
     );
   }
 }
-
-
-// style my components
-const styles = {
-  searchContainer: {
-    marginTop: 30,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 10
-  },
-  searchText: {
-    marginBottom: 10
-
-  },
-  searchButton: {
-    width: 100,
-    backgroundColor: 'blue',
-    alignSelf: 'center'
-
-  }
-};
---
+++ youtube-search/src/components/index.js
@@ -0,0 +1,3 @@
+import SearchBar from './SearchBar.js';
+
+export { SearchBar };
